7e5f1894f78a5f925af449f86de27fadf2424612
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-15 23:00:39 +0700

    Update gets()-puts().cpp
    puts() และ gets() เอาไว้ใช้กับพวก สตริง โดยมันจะรับข้อความที่เราพิมพ์มาทั้งบรรทัดเลย

--- input-output/gets()-puts().cpp
+++ input-output/gets()-puts().cpp
@@ -4,7 +4,9 @@
 int main(){
 	char name[20];
 	
+	//input string in line (don't care spacebar)
 	gets(name);
 	
-	printf("%s", name);
+	//display string, put address of string into puts(), but don't care about it array of char is already an address 📬
+	puts(name);
 }
