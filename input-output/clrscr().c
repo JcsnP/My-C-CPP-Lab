e2df6dfffc1ed797cb33705a27f8125d10a6f0b0
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-17 00:34:58 +0700

    Create clrscr().c

---
+++ input-output/clrscr().c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+	printf("------------------------------\n");
+	char e;
+	scanf("%s", &e);
+	
+	printf("------------------------------\n");
+	
+	if(e == 'e'){
+		system("cls");
+	}
+}
