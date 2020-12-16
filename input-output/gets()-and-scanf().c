608bda9f2ffee3712ad5bf13a4105078d5dfd19c
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-16 22:27:35 +0700

    Create gets()-and-scanf().c

---
+++ input-output/gets()-and-scanf().c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	char name[100];
+	char lastname[100];		//last name
+	
+	scanf("%s %s", name, name);
+	
+	printf("%s and %s", name, name);
+	
+	/*	INPUT
+	 * 	Chitsanupong Paenyoi
+	 * 	
+	 * 	OUTPUT
+	 * 	Paenyoi and Paenyoi
+	 */
+	 
+	 //solution
+	 scanf("%s %s", name, lastname);
+	 
+	 printf("%s and %s", name, lastname);
+	 
+	 /*	INPUT
+	 * 	Chitsanupong Paenyoi
+	 * 	
+	 * 	OUTPUT
+	 * 	Chitsanupong and Paenyoi
+	 */
+}
