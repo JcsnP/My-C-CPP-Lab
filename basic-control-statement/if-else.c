d840346f27d0106287ff3a1f2506df24b5265975
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-16 23:07:35 +0700

    Create if-else.c

---
+++ basic-control-statement/if-else.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//typedef long int ll;
+
+int main(){
+	int age;
+	cin >> age;
+	
+	if(age > 18){
+		cout << "you're adult";
+	}else{
+		cout << "young boi";
+	}
+}
