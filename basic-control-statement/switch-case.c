7643795f3752c20efbc9489af707ccf887810513
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-16 23:47:15 +0700

    Create switch-case.c

---
+++ basic-control-statement/switch-case.c
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//typedef long int ll;
+
+
+int main(){
+	//switch case
+	char c; cin >> c;
+	
+	switch (c){
+		case 'A':
+			cout << "you press A\n";
+			break;
+		case 'B':
+			cout << "You press B\n";
+			break;
+		case 'C':
+			cout << "You press C\n";
+			break;
+		default:
+			cout << "You don't press A, B and C\n"; 
+	}
+}
