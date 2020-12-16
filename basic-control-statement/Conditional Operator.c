630fe8adf7bb07bbc6d29dede0fe3fb9d5b6cca7
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-16 23:30:40 +0700

    Create Conditional Operator.c

---
+++ basic-control-statement/Conditional Operator.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//typedef long int ll;
+
+
+int main(){
+	//Conditional Operator
+	int n; cin >> n;
+	
+	(n % 2 ==0) ? cout << "Even" : cout << "Odd";
+	
+	//cout << (n % 2 == 0 ? "Even" : "Odd");	competitive style 😎
+}
