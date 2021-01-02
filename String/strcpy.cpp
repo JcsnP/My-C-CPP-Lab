eebb06f3630264f11f7d7237c4851cbfda9340e8
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2021-01-03 00:24:52 +0700

    Create strcpy.cpp

---
+++ String/strcpy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	char phone_number[20] = {"xxx-xxx-xxxx"};
+	
+	strcpy(phone_number, "093-658-3928");
+	
+	cout << phone_number;
+}
+
+/* COMMENT
+use strcpy to copy value and give to other array
+*/
