c925e11101b9b3746ec148c56747af4364ba0377
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2021-01-21 13:35:54 +0700

    update - add description in file

--- My Library/pythagoras/pythagoras.h
+++ My Library/pythagoras/pythagoras.h
@@ -1,4 +1,9 @@
-// Library file
+/*
+  @ just test header file
+  made by jcsnp. i made because wanna test learn about header file.
+  this header file can't work in real work, just test.
+*/
+
 #include "pythagoras.cpp"
 
 double pythagoras(double a, double b);
