8f97c5aa3065c678e417b665acdf75f85a7d9ae2
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2021-05-26 18:59:26 +0700

    --update code

--- Class/constructor.cpp
+++ Class/constructor.cpp
@@ -14,16 +14,32 @@ class Car{
 		
 };
 
+class Employee{
+	public:
+		string firstName;
+		string lastName;
+		int id;
+		Employee(string x, string y, int z);
+};
+
+Employee::Employee(string x, string y, int z){
+	firstName = x;
+	lastName = y;
+	id = z;
+}
+
 int main(void){
 	// create car object and call the constructor with difference value
 	Car carObj1("BMW", "X5", 1999);
 	Car carObj2("Ford", "Mustang", 1969);
+	
+	Employee employee1("Chitsanupong", "Paenyoi", 3012);
+	Employee employee2("Michael", "Jackson", 3013);
 
 	cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << endl;
 	cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << endl;
+	
+	cout << employee1.firstName << " " << employee1.lastName << " " << employee1.id << endl;
+	cout << employee2.firstName << " " << employee2.lastName << " " << employee2.id << endl;
+	
 }
-
-/* OUTPUT
-BMW X5 1999
-Ford Mustang 1969
-*/
