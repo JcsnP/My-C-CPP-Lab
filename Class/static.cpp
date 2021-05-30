b5889fbcadc45c5998c7732bdf521aca35bc14f7
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2021-05-30 18:06:38 +0700

    --create file

---
+++ Class/static.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Human{
+	private:
+		static int population;
+	public:
+		string name;
+		int age;
+		void display(){
+			cout << "name: " << name << endl << "age: " << age;
+		};
+		Human(string x, int y){
+			name = x;
+			age = y;
+			++population;
+		}
+		int get_population(){
+			return population;
+		}
+};
+
+int Human::population = 0;
+
+int main(){
+	Human james("Chitsaupong", 19);
+	Human chef("Taworn", 19);
+	Human liam("Liam", 48);
+	
+	cout << "Population: " << liam.get_population();
+}
